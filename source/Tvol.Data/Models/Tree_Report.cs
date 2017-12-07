89a598ef14220c9739b3894942fbae96cf26ac88
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-12-07 18:04:07 -0500

    add Tree_reportID column

--- source/Tvol.Data/Models/Tree_Report.cs
+++ source/Tvol.Data/Models/Tree_Report.cs
@@ -7,21 +7,22 @@ using System.Threading.Tasks;
 
 namespace Tvol.Data
 {
+    [Table("Tree_Report")]
     public class Tree_Report
     {
         public static readonly string CREATE_TABLE =
-@"
-CREATE TABLE Tree_Report
+@"CREATE TABLE Tree_Report
 (
-    TreeID INTEGER,
-    ReportID INTEGER,
-    FOREIGN KEY (TreeID) REFERENCES Tree ON DELETE CASCADE,
-    FOREIGN KEY (ReportID) REFERENCES Report ON DELETE CASCADE,
-    UNIQUE (TreeID)
+    [Tree_ReportID] INTEGER PRIMARY KEY AUTOINCREMENT,
+    [TreeID] INTEGER,
+    [ReportID] INTEGER,
+    FOREIGN KEY ([TreeID]) REFERENCES Tree ON DELETE CASCADE,
+    FOREIGN KEY ([ReportID]) REFERENCES Report ON DELETE CASCADE,
+    UNIQUE ([TreeID])
 );";
 
         [Key]
-        public int RowID { get; set; }
+        public int Tree_ReportID { get; set; }
 
         public int TreeID { get; set; }
 
